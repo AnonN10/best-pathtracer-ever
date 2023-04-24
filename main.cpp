033745749ebac0abccc8329d3c89dcbb3a9468ad
AnonN10 <69523725+AnonN10@users.noreply.github.com>
2023-04-25 04:09:51 +0700

    different LDS, NEE+MIS sun sampling

--- main.cpp
+++ main.cpp
@@ -14,12 +14,14 @@
 
 #define PI 3.141592653589793238462643
 
+#define ENABLE_NEE
+
 //sky
 glm::vec3 sky_color = glm::vec3(0.2, 0.55, 1.0);
 glm::vec3 sun_color = glm::vec3(1.0, 0.6, 0.3);
-float sun_intensity = 7.0f;
+float sun_intensity = 50.0f;
 glm::vec3 sun_dir = glm::normalize(glm::vec3(2.0, -1.0, 1.5));
-float sun_angle = 3.1415926f*0.1f;
+float sun_angle = 3.1415926f*0.04f;
 glm::vec3 calc_sky_color(glm::vec3 dir)
 {
 	float dir_up_cosine = glm::dot(dir, glm::vec3(0, 1, 0));
@@ -39,6 +41,18 @@ struct Transform
 	
 	glm::mat4 Compose() {return glm::translate(translation) * glm::toMat4(rotation) * glm::scale(scaling);}
 };
+
+//generates local coordinates given a normal vector.
+//taken from "Building an Orthonormal Basis, Revisited", Duff et al.
+glm::mat3 BuildLocalCoords(const glm::vec3& n)
+{
+    float s = (n.z < 0.0)?-1.0:1.0;
+    float a = -1.0 / (s + n.z);
+    float b = n.x * n.y * a;
+    glm::vec3 t = glm::vec3(1.0 + s * n.x* n.x * a, s * b, -s * n.x);
+    glm::vec3 bt = glm::vec3(b, s + n.y * n.y * a, -n.y);
+    return glm::mat3(t, n, bt);
+}
 
 //intersection code
 struct IntersectionData
@@ -129,21 +143,12 @@ glm::uint WangHash(glm::uint seed)
 }
 
 //low discrepancy sequence
-glm::uvec2 Sobol(glm::uint n) {
-    glm::uvec2 p = glm::uvec2(0u);
-    glm::uvec2 d = glm::uvec2(0x80000000u);
-
-    for(; n != 0u; n >>= 1u) {
-        if((n & 1u) != 0u)
-            p ^= d;
-        
-        d.x >>= 1u; // 1st dimension Sobol matrix, is same as base 2 Van der Corput
-        d.y ^= d.y >> 1u; // 2nd dimension Sobol matrix
-    }
-    
-    return p;
+float RLDS(float phi_d, float d, float n) {
+	float a = 1.0f/glm::pow(phi_d, d);
+	return glm::fract(5.0f + a*n);
 }
 
+//owen scrambling
 glm::uint ReverseBits(glm::uint x) {
     x = ((x & 0xaaaaaaaau) >> 1) | ((x & 0x55555555u) << 1);
     x = ((x & 0xccccccccu) >> 2) | ((x & 0x33333333u) << 2);
@@ -167,6 +172,10 @@ glm::uint OwenScramble(glm::uint p, glm::uint seed) {
     return ReverseBits(p);
 }
 
+float OwenScrambleFloat(float p, glm::uint seed) {
+    return float(OwenScramble(glm::uint(p*float(0xffffffffu)), seed)) / float(0xffffffffu);
+}
+
 //sampling
 glm::vec3 MapToUnitSphere(glm::vec2 uv)
 {
@@ -183,6 +192,44 @@ glm::vec3 MapToUnitHemisphereCosineWeighted(glm::vec2 uv, glm::vec3 n)
 {
     glm::vec3 p = MapToUnitSphere(uv);
     return n+p;
+}
+
+float MapToUnitHemisphereCosineWeightedPDF(float cos_theta)
+{
+	return cos_theta / PI;
+}
+
+glm::vec3 MapToSolidAngle(const glm::vec2& uv, float theta_max)
+{
+	float phi = 2.0 * PI * uv.x;
+	float cos_theta = 1.0 - uv.y * (1.0 - glm::cos(theta_max));
+	float sin_theta = glm::sqrt(1.0 - cos_theta * cos_theta);
+	return glm::vec3(glm::cos(phi) * sin_theta, cos_theta, glm::sin(phi) * sin_theta);
+}
+
+float MapToSolidAnglePDF(float theta_max)
+{
+	return 1.0f/(2.0f * PI * (1.0f - glm::cos(theta_max)));
+}
+
+glm::vec3 SampleSkySun(const glm::vec2& uv, const glm::vec3& normal, const glm::mat3& sun_transform, float& cosine, float& PDF)
+{	
+	glm::vec3 dir = sun_transform * MapToSolidAngle(uv, sun_angle);
+	cosine = glm::clamp(glm::dot(normal, dir), 0.0f, 1.0f);
+	PDF = MapToSolidAnglePDF(sun_angle);
+    return dir;
+}
+
+float SampleSkySunPDF(glm::vec3 dir)
+{
+	float dir_sun_angle = glm::acos(glm::clamp(glm::dot(dir, -sun_dir), 0.0f, 1.0f));
+	return (dir_sun_angle < sun_angle)?MapToSolidAnglePDF(sun_angle):0.0f;
+}
+
+//MIS
+float BalanceHeuristic(float p_f, float p_g)
+{
+	return p_f/(p_f+p_g);
 }
 
 //etc
@@ -226,15 +273,24 @@ int main(int argc, char ** argv)
 	//rendering variables
 	Transform camera_transform;
 	glm::vec2 rotation_angles = glm::vec2(0, 0);
-	constexpr int max_bounces = 5;
-	glm::uvec2 sobol_seeds[max_bounces];	
+	constexpr int max_bounces = 4;
 	float sample_count = 0.0f;
+	glm::mat3 sun_transform = BuildLocalCoords(-sun_dir);
 	
-	for(int i = 0; i < max_bounces; ++i)
-		sobol_seeds[i] = glm::uvec2(
-			WangHash(glm::uint(i*2+0)),
-			WangHash(glm::uint(i*2+1))
-		);
+	//R LDS
+	constexpr glm::uint RLDS_d_max = max_bounces*2;
+	float RLDS_phi_d = 2.0f;
+	for(int i = 0; i < 15; ++i)
+	{
+		RLDS_phi_d = glm::pow(1.0f + RLDS_phi_d, 1.0f/float(RLDS_d_max+1));
+	}
+	//std::cout << "d_max = " << RLDS_d_max << std::endl;
+	//std::cout << "phi_d = " << RLDS_phi_d << std::endl;
+	
+	//Owen scrambling
+	glm::uint lds_scramble_seeds[max_bounces*2];
+	for(glm::uint i = 0; i < max_bounces*2; ++i)
+		lds_scramble_seeds[i] = WangHash(i);
  
     while (!quit)
     {
@@ -298,6 +354,10 @@ int main(int argc, char ** argv)
 			camera_transform.translation += camera_transform.rotation * (glm::vec3(0, 0, 1) * camera_transform.scaling);
 			reset_rendering = true;
 		}
+		//just reset rendering by holding
+		if (state[SDL_SCANCODE_R]) {reset_rendering = true;}
+		//got tired of rendering
+		if (state[SDL_SCANCODE_ESCAPE]) {quit = true; break;}
 		
 		if(reset_rendering)
 		{
@@ -313,23 +373,21 @@ int main(int argc, char ** argv)
 		glm::mat4 viewproj = proj * glm::inverse(view);
 		glm::mat4 invviewproj = glm::inverse(viewproj);
 		
-		constexpr int sobol_seq_length = 1;
-		glm::vec2 sobol_seq_points[max_bounces][sobol_seq_length];
-		for(int seq = 0; seq < max_bounces; ++seq)
-			for(int i = 0; i < sobol_seq_length; ++i)
+		constexpr int lds_seq_length = 1;
+		float lds_points[RLDS_d_max][lds_seq_length];
+		for(int d = 0; d < RLDS_d_max; ++d)
+			for(int i = 0; i < lds_seq_length; ++i)
 			{
-				glm::uvec2 ip = Sobol(frame_idx * sobol_seq_length + i);
-				ip.x = OwenScramble(ip.x, sobol_seeds[seq][0]);
-				ip.y = OwenScramble(ip.y, sobol_seeds[seq][1]);
-				sobol_seq_points[seq][i] = glm::vec2(ip) / float(0xffffffffu);
+				lds_points[d][i] = RLDS(RLDS_phi_d, d+1, frame_idx * lds_seq_length + i);
+				//lds_points[d][i] = OwenScrambleFloat(lds_points[d][i], lds_scramble_seeds[d]);
 			}
 		
 		/*sample_count = 1.0f;
-		glm::ivec2 sobol_coord = glm::ivec2(sobol_seq_points[1][0] * glm::vec2(win_width, win_height));
-		if(sobol_coord.x < win_width && sobol_coord.x >= 0 && sobol_coord.y < win_height && sobol_coord.y >= 0)
+		glm::ivec2 lds_coord = glm::ivec2(glm::vec2(lds_points[2][0], lds_points[3][0]) * glm::vec2(win_width, win_height));
+		if(lds_coord.x < win_width && lds_coord.x >= 0 && lds_coord.y < win_height && lds_coord.y >= 0)
 		{
-			color_buffer[sobol_coord.x+win_width*sobol_coord.y] += glm::vec4(glm::vec3(0.5f), 1.0f);
-		}*/		
+			color_buffer[lds_coord.x+win_width*lds_coord.y] += glm::vec4(glm::vec3(0.5f), 1.0f);
+		}*/
 		
 		for(int y = 0; y < win_height; ++y)
 		{
@@ -344,39 +402,70 @@ int main(int argc, char ** argv)
 				//obtain NDC space screen coordinate
 				glm::vec2 ndc_xy = glm::vec2(float(x)/float(win_width)*2.0-1.0, -(float(y)/float(win_height)*2.0-1.0));
 				//add jitter for anti-aliasing
-				ndc_xy += sobol_seq_points[0][0]/glm::vec2(win_width, win_height)*2.0f;
+				ndc_xy += glm::vec2(lds_points[0][0], lds_points[1][0])/glm::vec2(win_width, win_height)*2.0f;
 				
 				//primary ray
 				glm::vec3 ray_orig = UnprojectNDC(glm::vec3(ndc_xy, 0.0f), invviewproj);
 				glm::vec3 ray_dir = UnprojectNDC(glm::vec3(ndc_xy, 1.0f), invviewproj);
 				ray_dir = glm::normalize(ray_dir - ray_orig);
 				
-				//fetch blue noise mask value for this pixel
-				glm::vec2 blue_noise = glm::vec2(blue_noise_pixels[x%blue_noise_width + y%blue_noise_height*blue_noise_height]);
 				
 				for(int bounce = 0; bounce < max_bounces; ++bounce)
 				{
 					IntersectionData isect_data = TraceScene(ray_orig, ray_dir);
 					glm::vec3 albedo = glm::vec3(1.0);
+					float cosine;
 					if(isect_data.hit())
 					{
-						//blue noise dithering of the low discrepancy sequence
-						glm::vec2 rand_uv = glm::fract(sobol_seq_points[bounce][0] + blue_noise);
+						//fetch blue noise mask value for this bounce
+						glm::vec2 blue_noise = glm::vec2(blue_noise_pixels[(x+1324*bounce)%blue_noise_width + (y+764352*bounce)%blue_noise_height*blue_noise_height]);
+						//do the blue noise dithering of the low discrepancy sequence
+						glm::vec2 rand_uv = glm::fract(glm::vec2(lds_points[bounce*2+0][0], lds_points[bounce*2+1][0]) + blue_noise);
 						
-						//make new ray
+						//make new ray for a higher dimensional integral of the BRDF estimator
 						ray_orig = ray_orig + ray_dir * isect_data.t;
 						ray_orig += isect_data.normal * 0.0005f;//ray bias
 						ray_dir = glm::normalize(MapToUnitHemisphereCosineWeighted(rand_uv, isect_data.normal));
+						cosine = glm::clamp(glm::dot(isect_data.normal, ray_dir), 0.0f, 1.0f);
 						
-						float cosine = glm::dot(isect_data.normal, ray_dir);
-						//pretty much all the terms are canceled out by the cosine weighted hemisphere sampling with PDF=cosine/PI,
-						//would have been this otherwise: cosine * radiance * albedo/PI / PDF
+#ifdef ENABLE_NEE
+						//sample the sun disk in the sky directly with a Next Event Estimator
+						float nee_cosine;
+						float nee_pdf;
+						glm::vec3 nee_ray_dir = SampleSkySun(
+							/*glm::vec2(
+								OwenScrambleFloat(rand_uv.x, lds_scramble_seeds[bounce*2+0]),
+								OwenScrambleFloat(rand_uv.y, lds_scramble_seeds[bounce*2+1])
+							)*/rand_uv,
+							isect_data.normal,
+							sun_transform,
+							nee_cosine,
+							nee_pdf
+						);
+						bool is_light_visible = !TraceScene(ray_orig, nee_ray_dir).hit();
+						//adding bounce!=max_bounces-1 condition makes the image match with the image without NEE,
+						//otherwise NEE is always 1 bounce ahead of the renderer without it, making it impossible to validate
+						if(is_light_visible && nee_cosine > 0 && bounce!=max_bounces-1)
+						{
+							//compute Multiple Importance Sampling weight and add the weighted contribution
+							float nee_mis_weight = BalanceHeuristic(nee_pdf, MapToUnitHemisphereCosineWeightedPDF(nee_cosine));
+							color += nee_mis_weight * throughput * (albedo/float(PI)) * nee_cosine * calc_sky_color(nee_ray_dir) / nee_pdf;
+						}
+#endif
+						
+						//pretty much all the terms are canceled out by the cosine weighted hemisphere sampling with BRDF=albedo/PI and PDF=cosine/PI,
+						//would have been this otherwise: cosine * BRDF / PDF
 						throughput *= albedo;
 					}
 					else
 					{
-						//hit nothing, so fetch the sky color and multiply by the portion of how much light goes to the sensor
-						color += throughput * calc_sky_color(ray_dir);
+						//the ray didn't hit any geometry, so fetch the sky color and multiply by the portion of how much light goes to the sensor
+#ifdef ENABLE_NEE
+						float mis_weight = bounce!=0? BalanceHeuristic(MapToUnitHemisphereCosineWeightedPDF(cosine), SampleSkySunPDF(ray_dir)): 1.0f;						
+						color += mis_weight * throughput * calc_sky_color(ray_dir);
+#else
+						color += throughput * calc_sky_color(ray_dir);						
+#endif
 						break;
 					}
 				}
